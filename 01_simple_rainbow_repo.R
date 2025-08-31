a9e99c39745932afa6318024adfe7b225e933978
PaulineDataWard <polo@paulineward.me.uk>
2025-08-31 19:52:13 +0100

    Code to create red data

---
+++ 01_simple_rainbow_repo.R
@@ -0,0 +1,15 @@
+# Simple examples by Pauline for
+# following along with: Learning Git by Anna Skoulikari
+
+# Red, colour 1 ---
+red_text <- "Red is the first colour of the rainbow."
+typeof(red_text) # "character"
+
+just_one_el <- 1L
+typeof(just_one_el) # "integer"
+
+red_one_point_one_decimal <- 1.1
+typeof(red_one_point_one_decimal) # "double"
+
+red_just_one <- 1
+typeof(red_just_one) # "double"
